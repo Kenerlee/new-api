b5284cf7d7a841fb5e89b5f7aa0495163419f8a3
Kaden Lau <141212002+Kenerlee@users.noreply.github.com>
2026-06-14 22:14:49 +0800

    go

--- setting/system_setting/system_setting_old.go
+++ setting/system_setting/system_setting_old.go
@@ -1,6 +1,6 @@
 package system_setting
 
-var ServerAddress = "http://localhost:3000"
+var ServerAddress = "https://api.moments.top"
 var WorkerUrl = ""
 var WorkerValidKey = ""
 var WorkerAllowHttpImageRequestEnabled = false
